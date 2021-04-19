196ca5918013e86a2ac5b3046944c05229c07f46
lucas <lcampbell0815@gmail.com>
2021-04-19 17:00:03 -0500

    added player basic movement

---
+++ Assets/Scripts/Gameplay/HealthBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float _health;
+    [SerializeField]
+    private bool _destroyOnDeath = true;
+
+    public float Health { get{ return _health; } }
+
+    public void TakeDamage(float damage)
+    {
+        _health -= damage;
+        if (_health < 0)
+            _health = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the object health is lower or equal to 0, destroy the object
+        if (_health <= 0 && _destroyOnDeath)
+            Destroy(gameObject);
+        else if (_health <= 0)
+            gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Gameplay/PlayerAttackBehaviour.cs
+++ Assets/Scripts/Gameplay/PlayerAttackBehaviour.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerAttackBehaviour : MonoBehaviour
 {
     public GameObject Target;
+    private NavMeshAgent _agent;
+    [SerializeField]
+    private float _attackRange = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Target)
+            return;
+
+        _agent.SetDestination(Target.transform.position);
+        float distance = (Target.transform.position - transform.position).magnitude;
+        if(distance < _attackRange)
+        {
+
+        }
     }
 }
